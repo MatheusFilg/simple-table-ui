2a4c1ab04429c67d34ee303a99d6709ba56875be
matheusf <matheus_falmeida@hotmail.com>
2025-02-02 13:37:32 -0300

    add tailwind v4 and initial setup

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@ import FirstTable from './components/FirstTable.vue'
 </script>
 
 <template>
-  <FirstTable />
+  <div class="fixed w-screen h-screen px-4 py-6">
+    <FirstTable />
+  </div>
 </template>
 
